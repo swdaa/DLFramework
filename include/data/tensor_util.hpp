43e8904bb360dc6c909343940956955c13ea308a
Song Wendong <swdailh@gmal.com>
2025-07-21 20:14:29 +1200

    addd runtime

--- include/data/tensor_util.hpp
+++ include/data/tensor_util.hpp
@@ -6,33 +6,28 @@
 namespace block_scholes
 {
 
-    std::tuple<sftensor, sftensor> TensorBroadcast(const sftensor &tensor1, const sftensor &tensor2);
-    std::shared_ptr<Tensor<float>> TensorPadding(const std::shared_ptr<Tensor<float>> &tensor, const std::vector<uint32_t> &pads, float padding_value);
-    bool TensorIsSame(const std::shared_ptr<Tensor<float>> &a, const std::shared_ptr<Tensor<float>> &b, float threshold = 1e-5f);
-    void TensorElementAdd(const std::shared_ptr<Tensor<float>> &tensor1,
-                          const std::shared_ptr<Tensor<float>> &tensor2,
-                          const std::shared_ptr<Tensor<float>> &output_tensor);
-    std::shared_ptr<Tensor<float>> TensorElementAdd(const std::shared_ptr<Tensor<float>> &tensor1, const std::shared_ptr<Tensor<float>> &tensor2);
-    void TensorElementMultiply(const std::shared_ptr<Tensor<float>>& tensor1,
-                           const std::shared_ptr<Tensor<float>>& tensor2,
-                           const std::shared_ptr<Tensor<float>>& output_tensor);
+std::tuple<sftensor, sftensor> TensorBroadcast(const sftensor &tensor1, const sftensor &tensor2);
+std::shared_ptr<Tensor<float>> TensorPadding(const std::shared_ptr<Tensor<float>> &tensor,
+                                             const std::vector<uint32_t> &pads, float padding_value);
+bool TensorIsSame(const std::shared_ptr<Tensor<float>> &a, const std::shared_ptr<Tensor<float>> &b,
+                  float threshold = 1e-5f);
+void TensorElementAdd(const std::shared_ptr<Tensor<float>> &tensor1, const std::shared_ptr<Tensor<float>> &tensor2,
+                      const std::shared_ptr<Tensor<float>> &output_tensor);
+std::shared_ptr<Tensor<float>> TensorElementAdd(const std::shared_ptr<Tensor<float>> &tensor1,
+                                                const std::shared_ptr<Tensor<float>> &tensor2);
+void TensorElementMultiply(const std::shared_ptr<Tensor<float>> &tensor1, const std::shared_ptr<Tensor<float>> &tensor2,
+                           const std::shared_ptr<Tensor<float>> &output_tensor);
 
-    std::shared_ptr<Tensor<float>> TensorElementMultiply(
-    const std::shared_ptr<Tensor<float>>& tensor1,
-    const std::shared_ptr<Tensor<float>>& tensor2);
+std::shared_ptr<Tensor<float>> TensorElementMultiply(const std::shared_ptr<Tensor<float>> &tensor1,
+                                                     const std::shared_ptr<Tensor<float>> &tensor2);
 
-    std::shared_ptr<Tensor<float>> TensorCreate(uint32_t channels, uint32_t rows,
-                                            uint32_t cols);
+std::shared_ptr<Tensor<float>> TensorCreate(uint32_t channels, uint32_t rows, uint32_t cols);
 
-    std::shared_ptr<Tensor<float>> TensorCreate(
-    const std::vector<uint32_t>& shapes);
+std::shared_ptr<Tensor<float>> TensorCreate(const std::vector<uint32_t> &shapes);
 
-   //dedp copy
-    std::shared_ptr<Tensor<float>> TensorClone(
-    std::shared_ptr<Tensor<float>> tensor);
+// dedp copy
+std::shared_ptr<Tensor<float>> TensorClone(std::shared_ptr<Tensor<float>> tensor);
 
-
-
-}
+}  // namespace block_scholes
 
 #endif
