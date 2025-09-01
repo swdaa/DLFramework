5e3c48617cf847a1cc6ed5d737a207695c4fa4c6
swdaa <swdailh@gmail.com>
2025-09-01 21:00:22 +1200

    add flatten

---
+++ src/layer/details/flatten.hpp
@@ -0,0 +1,22 @@
+
+
+#ifndef KUIPER_INFER_SOURCE_LAYER_DETAILS_FLATTEN_HPP_
+#define KUIPER_INFER_SOURCE_LAYER_DETAILS_FLATTEN_HPP_
+#include "layer/abstract/non_param_layer.hpp"
+namespace block_scholes {
+class FlattenLayer : public NonParamLayer {
+ public:
+  explicit FlattenLayer(int32_t start_dim, int32_t end_dim);
+
+  StatusCode Forward(const std::vector<std::shared_ptr<Tensor<float>>>& inputs,
+                     std::vector<std::shared_ptr<Tensor<float>>>& outputs) override;
+
+  static StatusCode CreateInstance(const std::shared_ptr<RuntimeOperator>& op,
+                                   std::shared_ptr<Layer<float>>& flatten_layer);
+
+ private:
+  int32_t start_dim_ = 0;
+  int32_t end_dim_ = 0;
+};
+} 
+#endif  
